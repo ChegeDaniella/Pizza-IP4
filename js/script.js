0b9b139a2af7407d6abd96d316727b5a79aa85e4
ChegeDaniella <chegedaniella@gmail.com>
2020-02-28 12:22:25 +0300

    calling value from html to jquery

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
     $(".slide").click(function(){
         $(".simim").slideToggle();
     });
+    $("#submit").click(function(){
+        alert($(".custom-select").val());
+    });
 });
+
+function Pizza(size,crust,toppings,prizes){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.prizes = prizes;
+}
