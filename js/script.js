fc0663d77798e404a096fd7084a89ed8e363e293
ChegeDaniella <chegedaniella@gmail.com>
2020-03-01 17:32:18 +0300

    functions for sizes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(".slide").click(function(){
         $(".simim").slideToggle();
     });
-    $("#submit").click(function(){
-        var size=($(".size").val());
-        var crust=($(".crust").val());
-        alert(size+" "+crust).val()
-    });
+    // $("#submit").click(function(){
+    //     var size=($(".size").val());
+    //     var crust=($(".crust").val());
+    //     alert(size+" "+crust).val()
+    // });
 });
 
 function Pizza(size,crust,toppings,prizes){
@@ -15,8 +15,24 @@ function Pizza(size,crust,toppings,prizes){
     this.toppings = toppings;
     this.prizes = prizes;
 }
-function size(){
-    this.small = 500;
-    this.medium =900;
-    this.large =1000;
+var size=["small","medium","large"]
+var amount = 0;
+var adder =0 ;
+var valueTaker;
+var selectedsize;
+
+function smallsize(){
+    valueTaker = document.getElementById("small").val();
+    selectedsize = size[0];
+    alert(selectedsize)
+}
+function mediumsize(){
+    valueTaker = document.getElementById("medium").val();
+    selectedsize = size[1];
+    alert(selectedsize)
+}
+function largesize(){
+    valueTaker = document.getElementById("large").val();
+    selectedsize = size[2];
+    alert(selectedsize)
 }
