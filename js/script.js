db1af1b7b6bd2f406e895d2d26a82338622a3f52
ChegeDaniella <chegedaniella@gmail.com>
2020-03-01 22:45:32 +0300

    adding prototype to size cost

--- js/script.js
+++ js/script.js
@@ -15,41 +15,51 @@ function Pizza(size,crust,toppings,prizes){
     this.toppings = toppings;
     this.prizes = prizes;
 }
-document.getElementById("button").onclick=function(){
-var size=["small","medium","large"]
-var amount = 0;
-var adder =0 ;
-var valueTaker;
-var selectedsize;
-
-function smallsize(){
-    valueTaker = document.getElementById("small").val();
-    selectedsize = size[0];
-    alert(selectedsize)
-}
-function mediumsize(){
-    valueTaker = document.getElementById("medium").val();
-    selectedsize = size[1];    
-}
-function largesize(){
-    valueTaker = document.getElementById("large").val();
-    selectedsize = size[2];  
-}
-function size(){
-    if (valueTaker==500){
-        amount=adder+500;
-        alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
-        amount =0;
-        selectedsize=null;
-    }else if (valueTaker==800){
-        amount = adder + 800;
-        alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
-        amount =0;
-        selectedsize=null;
-    }else if (valueTaker==1000){
-        amount = adder + 1000;
-        alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
-        amount =0;
-        selectedsize=null;
+Pizza.prototype.cost = function(){
+    var cost = 0;
+    if (this.size ==="small"){
+        cost = 500;
+    } else if (this.size === "medium"){
+        cost = 800;
+    }else {
+        cost= 1000;
     }
-}};
+}
+// document.getElementById("button").onclick=function(){
+// var size=["small","medium","large"]
+// var amount = 0;
+// var adder =0 ;
+// var valueTaker;
+// var selectedsize;
+
+// function smallsize(){
+//     valueTaker = document.getElementById("small").val();
+//     selectedsize = size[0];
+//     alert(selectedsize)
+// }
+// function mediumsize(){
+//     valueTaker = document.getElementById("medium").val();
+//     selectedsize = size[1];    
+// }
+// function largesize(){
+//     valueTaker = document.getElementById("large").val();
+//     selectedsize = size[2];  
+// }
+// function size(){
+//     if (valueTaker==500){
+//         amount=adder+500;
+//         alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
+//         amount =0;
+//         selectedsize=null;
+//     }else if (valueTaker==800){
+//         amount = adder + 800;
+//         alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
+//         amount =0;
+//         selectedsize=null;
+//     }else if (valueTaker==1000){
+//         amount = adder + 1000;
+//         alert("You have selected"+" "+selectedsize+" "+"pizza size and amount is"+" "+amount);
+//         amount =0;
+//         selectedsize=null;
+//     }
+// }};
